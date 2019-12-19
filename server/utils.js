d1f6bdb7a58772a1eae2943a48073a5cb27206d5
angelobzsouza <angelo.bz.souza@gmail.com>
2019-12-18 21:37:21 -0300

    fix db heroku

--- server/utils.js
+++ server/utils.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
 
 exports.createMongoConnection = async () => {
-  console.log(process.env.MONGODB_URI);
   const uri = `${process.env.MONGODB_URI || 'mongodb://localhost/cloud-broker-ufscar'})`;
   const mongoConnection = new MongoClient(uri, { 
     useUnifiedTopology: true 
   });
+  console.log(mongoConnection);
   const mongodb = (await mongoConnection.connect()).db();
 
   return [mongodb, mongoConnection]
