523b4631519a4608baa5d9c6abee916253bb4d8a
angelobzsouza <angelo.bz.souza@gmail.com>
2019-12-18 22:09:43 -0300

    fix db heroku

--- server/utils.js
+++ server/utils.js
@@ -1,12 +1,8 @@
 const { MongoClient } = require('mongodb');
 
 exports.createMongoConnection = async () => {
-  const uri = `${process.env.MONGODB_URI || 'mongodb://localhost/cloud-broker-ufscar'})`;
-  const mongoConnection = new MongoClient(uri);
-  mongoConnection.connect((err) => {
-    console.log('Erro de banco')
-    console.log(err);
-  });
+  const uri = `${process.env.MONGODB_URI || 'mongodb://localhost/cloud-broker-ufscar'}`;
+  const mongoConnection = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   const mongodb = (await mongoConnection.connect()).db();
 
   return [mongodb, mongoConnection]
